a323b565dd301f015d0db8a3dc8e123f96280eda
Lunaticsky-tql <2013599@mail.nankai.edu.cn>
2022-10-05 16:52:53 +0800

    Backup

--- look.sh
+++ look.sh
@@ -1,6 +1,6 @@
 hexo clean
 git add .
 git commit -m "Backup"
-git push origin main
+git push https://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@github.com/Lunaticsky-tql/blog_backup.git
 hexo g
 hexo s
